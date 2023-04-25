0443302e91f31e4a35e77b6a58794b8da46c604c
Xtarii <56205541+Xtarii@users.noreply.github.com>
2023-04-25 08:53:00 +0200

    Add files via upload

---
+++ assets/fisher_men/interact.py
@@ -0,0 +1,13 @@
+"""
+Denna scripten kommer att fungera som en interakt när man "interactar" med
+fiskebåtar som spawnas random på kartan, men det kommer bara att spawna en
+åt gången med en 1/3 att dem faktist spawnar.
+"""
+import os, styles
+
+
+
+# Här är start av denna filen när main kallas
+def main(player):
+    print("Ohy mate, what are yo doing ote heere nouw")
+    input()
