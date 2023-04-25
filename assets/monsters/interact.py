7d740a0f640e90f65478adb91e1585dd4019848e
Xtarii <56205541+Xtarii@users.noreply.github.com>
2023-04-25 08:53:27 +0200

    Add files via upload

---
+++ assets/monsters/interact.py
@@ -0,0 +1,13 @@
+"""
+Här är scripten som kommer att fungera som en interact event om man
+"interactar" med monster
+"""
+import os, styles
+
+
+
+
+# Start av filen när main kallas
+def main(player):
+    print("Wrooor!!!!")
+    input()
