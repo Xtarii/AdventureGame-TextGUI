56a48b72103be6a7c6392e45a9254e8640037b44
Xtarii <56205541+Xtarii@users.noreply.github.com>
2023-05-11 09:21:42 +0200

    Add files via upload

--- assets/monsters/interact.py
+++ assets/monsters/interact.py
@@ -9,5 +9,26 @@ import os, styles
 
 # Start av filen när main kallas
 def main(player):
-    print("Wrooor!!!!")
-    input()
+    """
+    Interaction med monster, det kommer att sluta i en fight
+
+
+    Så i denna kommer att köra en battle
+    """
+
+    # Story telling
+    print(styles.Colors["FAIL"] + """
+Jag använde mitt fiskespö.
+Men det stora monster verkade
+inte bry sig, trots allt - det
+monsteret såg ut att ha hud av
+pansar eller något annat super
+hårt material.
+
+
+Så jag startade min båt och slydde
+    """ + styles.Colors["ENDC"])
+
+
+    # Delay
+    input(styles.Colors["BLUE"] + "< continue/enter > " + styles.Colors["ENDC"])
